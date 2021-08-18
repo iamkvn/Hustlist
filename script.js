a1f4e36021fd212424794f4ed51b3deba7158d95
iamkevn <iamkevnrodriguez@gmail.com>
2021-08-18 04:13:57 -0400

    user can view items left

--- script.js
+++ script.js
@@ -1,31 +1,73 @@
 const log = console.log;
 
 let checkboxNum = 0;
+let itemsLeft = 0;
 
-// ELEMENTS
+//NOTES
+//if input is empty handle it
+//DRAG AND DROP MESSAGE DOESNT MOVE DOWN WHEN TODO LIST IS GETTING FULL
+
+//========================= ELEMENTS ============================//
 const todoForm = document.querySelector('.js-todo__form');
 const todoInput = document.querySelector('.js-todo__input');
 const todoList = document.querySelector('.js-todo__list');
+const todoItemsLeft = document.querySelector('.js-todo__items-left');
+const todoFilter = document.querySelector('.todo__filter');
+const filter = document.querySelector('.filter');
+const message = document.querySelector('.message');
 
-// FUNCTIONS
+//========================= FUNCTIONS ============================//
 
-const task = (e) => {
-  const event = e.target;
+const updateItemsLeft = () => {
+  todoItemsLeft.textContent = `${itemsLeft} items left`;
 
+  itemsLeft === 0
+    ? todoFilter.classList.add('u-display')
+    : todoFilter.classList.remove('u-display');
+
+  itemsLeft === 1
+    ? (todoItemsLeft.textContent = `${itemsLeft} item left`)
+    : `${itemsLeft} items left`;
+};
+
+const display = () => {
+  todoFilter.classList.remove('u-display');
+  filter.classList.remove('u-display');
+  message.classList.add('u-display');
+};
+
+const completeTask = (event) => {
   if (event.type === 'checkbox') {
     const task = event.nextElementSibling.nextElementSibling;
     task.classList.toggle('u-complete');
   }
+};
 
+const deleteTask = (event) => {
   if (event.classList[0] === 'todo__icon') {
     const item = event.parentElement;
     item.remove();
+    itemsLeft--;
+    updateItemsLeft();
   }
+
+  if (itemsLeft === 0) {
+    message.classList.remove('u-display');
+  }
+};
+
+const task = (e) => {
+  const event = e.target;
+  completeTask(event);
+  deleteTask(event);
 };
 
 const createTask = (e) => {
   e.preventDefault();
+  itemsLeft++;
+
   checkboxNum++;
+
   const html = `
     <li class="todo__item js-todo__item">
         <input
@@ -41,14 +83,18 @@ const createTask = (e) => {
   `;
   todoList.insertAdjacentHTML('beforeend', html);
   todoForm.reset();
-  //   const todoCheckbox = document.querySelectorAll('.js-todo__checkbox');
-  //   const todoTask = document.querySelector('.js-todo__task');
+
   const todoItem = document.querySelectorAll('.js-todo__item');
   todoItem.forEach((item) => {
     item.addEventListener('click', task);
   });
+  updateItemsLeft();
+
+  if (itemsLeft === 1) {
+    display();
+  }
 };
 
-// EVENT LISTENERS
+//========================= EVENT LISTENERS ============================//
 
 todoForm.addEventListener('submit', createTask);
